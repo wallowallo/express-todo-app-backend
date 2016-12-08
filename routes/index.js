34a24e6e2836d8f92aed0db3bd5ad99598a7e942
Krister <krister.torsvik@gmail.com>
2016-12-08 11:51:58 +0100

    changed response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.jsonp({a:1, b:'hello'});
 });
 
 module.exports = router;
